366cdcf4a2bbab5f8adf3990ddd949e3531df6a9
Zach Eriksen <zmeriksen@icloud.com>
2022-04-05 19:13:18 -0600

    Implement basic ScreenDataApp for iOS and Watch

---
+++ iOS/Demo/Demo WatchKit Extension/DemoApp.swift
@@ -0,0 +1,23 @@
+//
+//  DemoApp.swift
+//  Demo WatchKit Extension
+//
+//  Created by Leif on 4/5/22.
+//
+
+import ScreenData
+import ScreenDataApp
+import ScreenDataNavigation
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    let screenDataApp = DefaultScreenDataApp(
+        baseID: "index",
+        screenProvider: URLScreenProvider(
+            baseURL: URL(string: "http://localhost:8080/")!
+        )
+    )
+    
+    var body: some Scene { screenDataApp.body }
+}
